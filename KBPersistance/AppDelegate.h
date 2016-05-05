bced89dd20ddd02b4898a005085f6ca8f106d327
kobi_2016 <254633397@qq.com>
2016-05-05 10:18:02 +0800

    tt

---
+++ KBPersistance/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KBPersistance
+//
+//  Created by chengshenggen on 5/5/16.
+//  Copyright © 2016 Gan Tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
